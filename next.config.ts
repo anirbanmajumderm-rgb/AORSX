450c8f296f86d3ad7020adb22f9e491659d887bc
Anirban Majumder <anirbanmajumderm@gmail.com>
2026-06-07 22:21:51 +0600

    fix dependency + stable build

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  turbopack: {},
+
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
